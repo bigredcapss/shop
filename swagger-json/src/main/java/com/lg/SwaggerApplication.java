b66701d62e85f21d4e1df9214bc1e0bcd5eaac00
BigRedCaps_Pro <bigredcaps@gmail.com>
2021-04-18 18:00:15 +0800

    基于SpringCloud Alibaba的云商城项目搭建

---
+++ swagger-json/src/main/java/com/lg/SwaggerApplication.java
@@ -0,0 +1,8 @@
+package com.lg;
+import com.lg.code.build.TemplateBuilder;
+public class SwaggerApplication {
+
+    public static void main(String[] args) {
+        TemplateBuilder.builder();
+    }
+}
---
+++ swagger-json/src/main/java/com/lg/code/build/SwaggerBuilder.java
@@ -0,0 +1,22 @@
+package com.lg.code.build;
+
+import java.util.Map;
+
+public class SwaggerBuilder {
+
+    /***
+     * ServiceImpl构建
+     * @param modelMap
+     */
+    public static void builder(Map<String,Object> modelMap){
+        //swagger的文件名字
+        modelMap.put("Table","swagger");
+
+        //生成ServiceImpl层文件
+        BuilderFactory.builder(modelMap,
+                "/template/swagger",
+                "swagger.json",
+                TemplateBuilder.SWAGGERUI_PATH,
+                ".json");
+    }
+}
